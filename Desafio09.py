415073885c1f16eb34f604acca0fea58e157cd3d
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-15 12:53:10 -0300

    refazendo desafio

--- Desafio09.py
+++ Desafio09.py
@@ -1,22 +1,18 @@
-reta_1= float(input('Digite aqui o comprimento da primeira reta:'))
-reta_2= float(input('Digite aqui o comprimento da segunda reta:'))
-reta_3= float(input('Digite aqui o comprimento da terceira reta:'))
-if reta_1+reta_2>reta_3 and reta_3+reta_1>reta_2 and reta_2+reta_3>reta_1:
-    print('As medidas acima formam um triângulo!')
-    if reta_1==reta_2==reta_3:
-        print('As reta são iguais e formam um triângulo Equilátero!')
-    if reta_1!=reta_2!=reta_3!=reta_1:
-        print('Todos os lados são diferentes, formando um triângulo Escaleno!')
+r1= float(input('Digite aqui o comprimento da primeira reta:'))
+r2= float(input('Digite aqui o comprimento da segunda reta:'))
+r3= float(input('Digite aqui o comprimento da terceira reta:'))
+if r1+r2>r3 and r1+r3>r2 and r2+r3>r1:
+    print('As retas podem formar um triângulo')
+    if r1==r2==r3:
+        print('Equilátero: todos os lados são iguais')
+    elif r1!=r2!=r3!=r1:
+        print('Escaleno: todos os lados são diferentes')
     else:
-        print('As medidas formam um triângulo Isósceles, pois dois lados são iguais!')
-print('Tenha um bom dia!')
-
-
-
-
+        print('Isósceles: dois lados são iguais')
+print('Fim do programa')
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:
-Equilátero:todos os lados são iguais
+Equilátero:todos os lados são iguais4
 Isósceles: dois lados iguais
 Escaleno:Todos os lados diferentes
 '''

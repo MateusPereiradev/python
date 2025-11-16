fd9246b047ee7fef33c90817412705aad70d6257
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-16 00:58:39 -0300

    melhorando desafio08

--- Desafio08.py
+++ Desafio08.py
@@ -9,6 +9,15 @@ metodo_pagamento= int(input('Digite aqui a opção de pagamento desejada de 1 a 
 if metodo_pagamento==1:
     valor_desconto= valor_produto-valor_produto*0.10
     print(f'Referente ao valor do produto R${valor_produto:.2f}, com 10% de desconto ficará R${valor_desconto:.2f}')
+elif metodo_pagamento==2:
+    valor_desconto=valor_produto-valor_produto*0.05
+    print(f'Referente ao valor do produto R${valor_produto:.2f}, com 5% de desconto ficará R${valor_desconto:.2f}')
+elif metodo_pagamento==3:
+    print(f'Referente ao valor do produto R${valor_produto:.2f}, em até 2x no cartão o preço será o normal.')
+else:
+    valor_juros= valor_produto+valor_produto*0.30
+    print(f'Referente ao valor do produto R${valor_produto:.2f}, em 3x ou mais no cartão terá 20% de juros, ficando R${valor_juros:.2f}')
+print('Obrigado por comprar na nossa LOJA PYTHON! Volte Sempre!')    
 
 '''
 Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:

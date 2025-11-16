56c9b760100b4fefb87f63b2e0d36a87d78254f8
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-16 13:03:20 -0300

    treinando condições aninhadas

--- Desafio09.py
+++ Desafio09.py
@@ -1,15 +1,18 @@
-r1= float(input('Digite aqui o comprimento da primeira reta:'))
-r2= float(input('Digite aqui o comprimento da segunda reta:'))
-r3= float(input('Digite aqui o comprimento da terceira reta:'))
-if r1+r2>r3 and r1+r3>r2 and r2+r3>r1:
-    print('As retas podem formar um triângulo')
+print('===== Analisador de Triângulos =====')
+nome= str(input('Digite aqui o seu nome:')).strip()
+r1= float(input('Digite aqui a primeira reta:'))
+r2= float(input('Digite aqui a segunda reta:'))
+r3= float(input('Digite aqui a terceira reta:'))
+if r1+r2>r3 and r1+r3>r3 and r2+r3>r1:
+    print(f'Referente as retas {r1}m {r2} e {r3}, é possível formar um triângulo!')
     if r1==r2==r3:
-        print('Equilátero: todos os lados são iguais')
+        print(f'Referente as retas {r1}, {r2} e {r3}, é possível formar um triângulo EQUILÁTERO!')
     elif r1!=r2!=r3!=r1:
-        print('Escaleno: todos os lados são diferentes')
+        print(f'Referente as retas {r1}, {r2} e {r3}, é possível formar um triângulo ESCALENO!')
     else:
-        print('Isósceles: dois lados são iguais')
-print('Fim do programa')
+        print(f'Referente as retas {r1}, {r1} e {r3}, é possível formar um triângulo ISÓSCELES!')
+print(f'Muito obrigado por utilizar o Analisador de Triângulos, {nome}! Volte Sempre!')
+
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:
 Equilátero:todos os lados são iguais4

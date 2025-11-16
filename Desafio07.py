987ba8b66f7feedd2cc55b690f5721c4e5441430
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-15 23:52:58 -0300

    aprimorando exercicio 07

--- Desafio07.py
+++ Desafio07.py
@@ -1,20 +1,21 @@
+from time import sleep
+nome= str(input('Digite aqui seu nome:')).strip()
 peso= float(input('Digite aqui seu peso:'))
 altura= float(input('Digite aqui sua altura:'))
-nome= str(input('Digite aqui seu nome:'))
 imc= peso/altura**2
-if imc <18.5:
-    print(f'Seu imc é {imc}, você está abaixo do peso!')
-elif 18.5 <= imc <=25:
-    print(f'Seu imc é {imc}, você está com o peso ideal!')
-elif 25 <= imc <=30:
-    print(f'Seu imc é {imc}, você está com sobrepeso!')
-elif 30 <= imc <= 40:
-    print(f'Seu imc é {imc}, você está com obesidade!')
+print('Calculando seu IMC...')
+sleep(3)
+if imc<18.5:
+    print(f'Seu IMC é {imc:.2f}, você está ABAIXO DO PESO.')
+elif 18.5<=imc<25:
+    print(f'Seu IMC é {imc:.2f}, você está no PESO IDEAL.')
+elif 25<=imc<30:
+    print(f'Seu IMC é {imc:.2f}, você está em SOBREPESO.')
+elif 30<=imc<40:
+    print(f'Seu IMC é {imc:.2f}, você está em OBESIDADE.')
 else:
-    print(f'Seu imc é {imc}, você está com obesidade mórbida!')
+    print(f'Seu IMC é {imc:.2f}, você está em OBESIDADE MÓRBIDA;')
 print(f'Tenha uma boa noite {nome}!')
-
-
 
 '''
 Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a

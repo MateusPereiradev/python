65d79d7b6741ac768a41048e86db381cbf937c3e
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-17 10:57:48 -0300

    refazendo exercicio para treinar

--- Desafio05.py
+++ Desafio05.py
@@ -1,18 +1,19 @@
-n1= float(input('Digite aqui a primeira nota do aluno:'))
-n2= float(input('Digite aqui a segunda nota do aluno:'))
-nome= str(input('Digite aqui o nome do aluno:'))
-media= (n1+n2)/2
-if media <5:
-    print(f'A média do aluno é {media}, o aluno está REPROVADO!')
+nome= str(input('Digite aqui seu nome:')).strip().capitalize()
+nota_1= float(input('Digite aqui a primeira nota:'))
+nota_2= float(input('Digite aqui a segunda nota:'))
+media= (nota_1+nota_2)/2
+if media < 5.0:
+    print(f'Sua média é de {media}, voce foi REPROVADO!')
 elif 5.0<= media <=6.9:
-    print(f'A média do aluno é {media}, o aluno está de RECUPERAÇÃO!')
-else:
-    print(f'A media do aluno é {media}, o aluno está APROVADO!')
-print(f'Tenha uma boa noite {nome}!')
+    print(f'Sua média é de {media}, você está de RECUPERAÇÃO!')
+elif media >= 7.0:
+    print(f'Sua média é de {media}, você está APROVADO!')
+print(f'Tenha uma ótima tarde {nome}')
+
 
 '''
 Crie um programa que leia duas notas e calcule sua média, mostrando uma mensagem no final, de acordo com a média
-atigida:
+atigida: 
 Média abaixo de 5.0:REPROVADO
 Média entre 5.0 e 6.9 RECUPERAÇÃO
 Média entre 7.0 ou superior:APROVADO

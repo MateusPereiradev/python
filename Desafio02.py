f2927f5eefa7d4b40f8c4814d8fd832428ff01b9
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-17 10:58:58 -0300

    praticando exercicio02

--- Desafio02.py
+++ Desafio02.py
@@ -1,16 +1,22 @@
+from time import sleep
+
+nome= str(input('Digite aqui seu nome:')).strip().capitalize()
 num= int(input('Digite aqui um número inteiro qualquer:'))
 print('''Escolha a base de conversão:
-      [1] para binário
+      [1] Para binário
       [2] para octal
       [3] para hexadecimal''')
-escolha_base= int(input('Escolha aqui a base de conversão de 1 a 3:'))
-if escolha_base==1:
-    print(f'Referente ao número {num}, em binário será {bin(num)[2:]}')
-elif escolha_base==2:
-    print(f'Referente ao número {num}, em octal será {oct(num)[2:]}')
-elif escolha_base==3:
-    print(f'Referente ao número {num}, em hexadecimal será {hex(num)[2:]}')
-print('Fim do programa')
+escolher_base= int(input('Digite aqui a sua escolha de 1 a 3:'))
+print('Calculando a base escolhida...')
+sleep(3)
+if escolher_base==1:
+    print(f'Referente ao número {num}, o mesmo convertido para BINÁRIO é: {bin(num)[2:]}')
+elif escolher_base==2:
+    print(f'Referente ao número {num}, o mesmo convertido para OCTAL é: {oct(num)[2:]}')
+elif escolher_base==3:
+    print(f'Referente ao número {num}, o mesmo convertido para HEXADECIMAL é: {hex(num)[2:]}')
+print(f'Tenha uma bom dia {nome}!!')
+
 
 '''
 Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher

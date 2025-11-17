33dfffd0cb6674b2ac019f0fa1cea73c4c6b71f1
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-16 21:34:17 -0300

    Atualizando Desafio09

--- Desafio09.py
+++ Desafio09.py
@@ -15,7 +15,7 @@ print(f'Muito obrigado por utilizar o Analisador de Triângulos, {nome}! Volte S
 
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:
-Equilátero:todos os lados são iguais4
+Equilátero:todos os lados são iguais
 Isósceles: dois lados iguais
 Escaleno:Todos os lados diferentes
 '''

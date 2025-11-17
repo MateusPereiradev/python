b267288e09089bc4c04468fe538ed887868bc85c
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-17 11:24:41 -0300

    reforçando mais o aprendizado

--- Desafio09.py
+++ Desafio09.py
@@ -1,18 +1,19 @@
-print('===== Analisador de Triângulos =====')
-nome= str(input('Digite aqui o seu nome:')).strip()
-r1= float(input('Digite aqui a primeira reta:'))
-r2= float(input('Digite aqui a segunda reta:'))
-r3= float(input('Digite aqui a terceira reta:'))
-if r1+r2>r3 and r1+r3>r3 and r2+r3>r1:
-    print(f'Referente as retas {r1}m {r2} e {r3}, é possível formar um triângulo!')
-    if r1==r2==r3:
-        print(f'Referente as retas {r1}, {r2} e {r3}, é possível formar um triângulo EQUILÁTERO!')
-    elif r1!=r2!=r3!=r1:
-        print(f'Referente as retas {r1}, {r2} e {r3}, é possível formar um triângulo ESCALENO!')
-    else:
-        print(f'Referente as retas {r1}, {r1} e {r3}, é possível formar um triângulo ISÓSCELES!')
-print(f'Muito obrigado por utilizar o Analisador de Triângulos, {nome}! Volte Sempre!')
+from time import sleep
 
+r1= float(input('Digite aqui a medida da primeira reta:'))
+r2= float(input('Digite aqui a medida da segunda reta:'))
+r3= float(input('Digite aqui a medida da terceira reta:'))
+print('Calculando medidas...')
+sleep(3)
+if r1+r2>r3 and r2+r3>r1 and r1+r3>r2:
+    print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo!')
+    if r1==r2==r3:
+        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triâgulo do tipo EQUILÁTERO!')
+    elif r1!=r2!=r3!=r1:
+        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo do tipo ESCALENO!')
+    else:
+        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo ISÓSCELES!')
+print('Fim do código')
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:
 Equilátero:todos os lados são iguais

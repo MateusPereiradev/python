76a793174bf12a780ba1d5279b79241b5301b0e9
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-17 13:20:33 -0300

    adicionando mais funcionalidades

--- Desafio10.py
+++ Desafio10.py
@@ -1,10 +1,10 @@
 from random import choice
 from time import sleep
 print('===== JOKENPÔ =====')
-print('Escolha sua jogada:' \
-'Pedra' \
-'Papél' \
-'Tesoura')
+print('''Escolha sua jogada:
+      Pedra
+      Papel
+      Tesoura''')
 jogador= str(input('Qual é a sua jogada?')).strip().capitalize()
 itens= ['Pedra','Papel', 'Tesoura' ]
 computador= choice(itens)
@@ -14,8 +14,10 @@ if computador==jogador:
     print(f'A escolha do jogador foi {jogador} e a escolha do computador foi {computador}, sendo assim houve um EMPATE!')
 elif computador=='Pedra' and jogador== 'Tesoura' or computador== 'Papel' and jogador== 'Pedra' or computador=='Tesoura' and jogador=='Papel':
     print(f'Referente a jogada do computador {computador} e a do jogador {jogador}, o computador VENCEU!')
-else:
+elif jogador== 'Pedra' and computador=='Tesoura' or jogador=='Papel' and computador=='Pedra' or jogador== 'Tesoura' and computador=='Papel':
     print(f'Referente a jogada do computador {computador}, e a jogada do jogador {jogador}, o jogador VENCEU!')
+else:
+    print('Jogada inválida! Tente novamente.')
 print('Obrigado por jogar nosso jogo JOKENPÔ!')
 
 '''

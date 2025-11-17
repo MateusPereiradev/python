7944f1a905f94d9c7568f28f1f2e224d76b1df21
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-17 10:58:41 -0300

    praticando exercicio 03

--- Desafio03.py
+++ Desafio03.py
@@ -1,12 +1,12 @@
 num1= int(input('Digite aqui o primeiro número inteiro:'))
 num2= int(input('Digite aqui o segundo número inteiro:'))
 if num1>num2:
-    print(f'O primeiro valor {num1} é maior')
+    print(f'O primeiro número {num1} é maior que o segundo número {num2}!')
 elif num2>num1:
-    print(f'O segundo valor {num2} é maior')
+    print(f'O segundo número {num2} é maior que o primeiro número {num1}!')
 else:
-    print(f'Referente aos dois valor {num1} e {num2}, não existe valor maior, ambos são iguais')
-
+    print(f'Referete ao número {num1} e o número {num2} os dois são iguais!')
+print('Fim do programa!')
 
 '''
 Escreva um programa que leia dois números inteiros e compare-os mostrando na tela uma mensagem:

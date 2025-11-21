6bce15924a519196b22cbc2716de01508eb11fa5
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-20 21:08:17 -0300

    exercitando

--- Desafio05.py
+++ Desafio05.py
@@ -1,15 +1,13 @@
-nome= str(input('Digite aqui seu nome:')).strip().capitalize()
-nota_1= float(input('Digite aqui a primeira nota:'))
-nota_2= float(input('Digite aqui a segunda nota:'))
+nota_1= float(input("Digite aqui a sua primeira nota:"))
+nota_2= float(input("Digite aqui a sua segunda nota:"))
 media= (nota_1+nota_2)/2
-if media < 5.0:
-    print(f'Sua média é de {media}, voce foi REPROVADO!')
+if media<5.0:
+    print(f"Sua média foi {media}, você foi REPROVADO!")
 elif 5.0<= media <=6.9:
-    print(f'Sua média é de {media}, você está de RECUPERAÇÃO!')
-elif media >= 7.0:
-    print(f'Sua média é de {media}, você está APROVADO!')
-print(f'Tenha uma ótima tarde {nome}')
-
+    print(f"Sua média foi {media}, você está de RECUPERAÇÃO!")
+elif media>=7.0:
+    print(f"Sua média foi {media}, você está APROVADO!")
+print("Tenha uma ótima noite!")
 
 '''
 Crie um programa que leia duas notas e calcule sua média, mostrando uma mensagem no final, de acordo com a média

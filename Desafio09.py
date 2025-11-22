c9523425deb7a12a85f09b182bdd2a200984de41
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-22 19:22:42 -0300

    aprimorando

--- Desafio09.py
+++ Desafio09.py
@@ -1,6 +1,15 @@
 reta_1= float(input('Digite aqui o comprimeiro da primeira reta:'))
 reta_2= float(input('Digite aqui o comprimento da segunda reta:'))
 reta_3= float(input('Digite aqui o comprimento da terceira reta:'))
+if reta_1+reta_2>reta_3 and reta_2+reta_3>reta_1 and reta_1+reta_3>reta_2:
+    print(f'As medidas de {reta_1}, {reta_2} e {reta_3} formam um triângulo')
+    if reta_1==reta_2==reta_3:
+        print('As medidas formam um triângulo Equilátero')
+    elif reta_1!=reta_2!=reta_3!=reta_1:
+        print('As medidas formam um triângulo Escaleno')
+    else:
+        print('As medidas formam um triângulo Isósceles')
+print('Tenha uma boa noite!')
 
 
 '''

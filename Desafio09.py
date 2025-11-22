7329f8fdeecc2fd998430c9da4b48bdf63e6f6ea
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-21 22:02:39 -0300

    refazendo

--- Desafio09.py
+++ Desafio09.py
@@ -1,19 +1,7 @@
-from time import sleep
+reta_1= float(input('Digite aqui o comprimeiro da primeira reta:'))
+reta_2= float(input('Digite aqui o comprimento da segunda reta:'))
+reta_3= float(input('Digite aqui o comprimento da terceira reta:'))
 
-r1= float(input('Digite aqui a medida da primeira reta:'))
-r2= float(input('Digite aqui a medida da segunda reta:'))
-r3= float(input('Digite aqui a medida da terceira reta:'))
-print('Calculando medidas...')
-sleep(3)
-if r1+r2>r3 and r2+r3>r1 and r1+r3>r2:
-    print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo!')
-    if r1==r2==r3:
-        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triâgulo do tipo EQUILÁTERO!')
-    elif r1!=r2!=r3!=r1:
-        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo do tipo ESCALENO!')
-    else:
-        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo ISÓSCELES!')
-print('Fim do código')
 
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:

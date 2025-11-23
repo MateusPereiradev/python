4ec890497deb2e817b786e779d65e2bcb0d7ead3
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-23 11:24:09 -0300

    refazendo_conversao

--- Desafio02.py
+++ Desafio02.py
@@ -1,16 +1,11 @@
-num= int(input('Digite um número inteiro qualquer:'))
-print('''Escolha uma das bases para conversão
-      [1] Converter para BINÁRIO
-      [2] Converter para OCTAL
-      [3] Converter para HEXADECIMAL''')
-escolha= int(input('Digite aqui a sua escolha:'))
-if escolha==1:
-    print(f'O número {num} convertido para BINÁRIO é igual a {bin(num)[2:]}')
-elif escolha==2:
-    print(f'O número {num} convertido para OCTAL é igual {oct(num)[2:]}')
-elif escolha==3:
-    print(f'O número {num} convertido para HEXADECIMAL é igual a {hex(num)[2:]}')
-print('Obrigado por utilizar nosso sistema de conversão de bases numéricas!')
+num=int(input('Digite aqui um número inteiro para ser feita a conversão:'))
+print('''Escolha aqui sua base de conversão:
+      [1] Binário
+      [2] Octal
+      [3] Hexadecimal''')
+escolha_usuario= int(input('Digite aqui a sua escolha para conversão do número inteiro:'))
+if escolha_usuario==1:
+    print(f'Referente ao número inteiro {num}, sua base convertida para Binário é {bin(num)[2:]}')
 
 '''
 Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher

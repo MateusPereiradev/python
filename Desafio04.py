dcdf417f66452b37ca3e4d572439f16443505915
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-24 11:30:51 -0300

    calculadora_alistamento

--- Desafio04.py
+++ Desafio04.py
@@ -1,17 +1,16 @@
+from datetime import date
 nascimento= int(input('Digite aqui o ano de seu nascimento:'))
-idade= 2025-nascimento
-falta= 18-idade
-passou= idade-18
+ano_atual= date.today().year
+idade=ano_atual-nascimento
+falta_alistar=18-idade
+passou_alistar= idade-18
 if idade<18:
-    print(f'''Sua idade é de {idade} anos, você ainda vai se alistar no exército!
-          Ainda faltam {falta} anos para se alistar''')
-elif idade==18:
-    print(f"Você tem {idade} anos, está na hora de se alistar!")
+    print(f'Você tem {idade} anos, ainda faltam {falta_alistar} anos para se alistar!')
+elif idade>18:
+    print(f'Você tem {idade} anos, já passou {passou_alistar} anos para se alistar!')
 else:
-    print(f'''Você tem {idade} anos, e já passou da hora de se alistar!!
-          Já passou {passou} anos para se alistar!''')
-print('Tenha uma boa noite!')
-
+    print(f'Você tem {idade} anos, está na hora de se alistar')
+print('Obrigado por usar nosso programa de conversão!')
 '''
 Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a idade:
 Se ele ainda vai se alistar ao serviço militar.

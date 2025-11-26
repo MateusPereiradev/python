0e4d866041b1d16127407ceee7f182305adaf849
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-26 19:48:15 -0300

    numeros_inteiros

--- Desafio03.py
+++ Desafio03.py
@@ -1,11 +1,11 @@
-num_1= int(input('Digite aqui um número inteiro:'))
-num_2= int(input('Digite aqui outro número inteiro:'))
+num_1= int(input('Digite aqui o primeiro número inteiro:'))
+num_2= int(input('Digite aqui o segundo número inteiro:'))
 if num_1>num_2:
-    print(f'O número {num_1} é maior que o número {num_2}!')
+    print(f'Referente ao primeiro número {num_1} e ao segundo número {num_2} o primeiro número é maior!')
 elif num_2>num_1:
-    print(f'O número {num_2} é maior que o número {num_1}!')
+    print(f'Referente ao primeiro número {num_1} e ao segundo número {num_2} o segundo número é maior!')
 else:
-    print(f'O número {num_1} é igual ao número {num_2}!')
+    print(f'Referente ao primeiro número {num_1} e ao segundo número {num_2} o dois números são iguais!')
 print('Fim do programa')
 
 '''

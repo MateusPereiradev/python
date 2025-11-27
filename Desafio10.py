cd21fa91f1e02d6ece2a2a771fefccfc9ad7eb07
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-26 21:59:29 -0300

    Jogando Jokenpô

--- Desafio10.py
+++ Desafio10.py
@@ -1,20 +1,19 @@
 from random import choice
 from time import sleep
-print('===== Jokenpô=====')
-print('''Escolha aqui sua jogada:
-      [1] Pedra
-      [2] Papel
-      [3] Tesoura''')
-escolha_jogador= str(input('Digite aqui sua escolha: Pedra, Papel ou Tesoura:')).strip().capitalize()
+print('''Escolha sua jogada:
+      Pedra
+      Papel
+      Tesoura''')
+jogador= str(input('Digite aqui a sua jogada Pedra, Papel ou Tesoura:')).strip().capitalize()
 computador= ['Pedra', 'Papel', 'Tesoura']
-escolha_computador= choice(computador)
-if escolha_jogador==escolha_computador:
-    print(f'A escolha do jogador foi {escolha_jogador} e a escolha do computador foi {escolha_computador}, sendo assim houve um EMPATE.')
-elif escolha_jogador=='Pedra' and escolha_computador=='Tesoura' or escolha_jogador=='Papel' and escolha_computador=='Pedra' or escolha_jogador=='Tesoura' and escolha_computador=='Papel':
-    print(f'A escolha do jogador foi {escolha_jogador} e a escolha do computador foi {escolha_computador}, sendo assim o JOGADOR GANHOU!!')
-elif escolha_computador=='Pedra' and escolha_jogador=='Tesoura' or escolha_computador=='Papel' and escolha_jogador=='Pedra' or escolha_computador=='Tesoura' and escolha_computador=='Papel':
-    print(f'A escolha do jogador foi {escolha_jogador} e a escolha do computador foi {escolha_computador}, sendo assim o COMPUTADOR GANHOU!')
-print('Obrigado por jogar nosso jogo!')
+jogada_computador= choice(computador)
+if jogador==jogada_computador:
+    print(f'Referente a jogada do jogador {jogador} e a do computador {jogada_computador} houve um EMPATE!')
+if jogador=='Pedra' and jogada_computador=='Tesoura' or jogador=='Papel' and jogada_computador=='Pedra' or jogador=='Tesoura' and jogada_computador=='Papel':
+    print(f'Referente a jogada do jogador {jogador} e a jogada do computador {jogada_computador} o JOGADOR VENCEU!')
+if jogada_computador=='Pedra' and jogador=='Tesoura' or jogada_computador=='Papel' and jogador=='Pedra' or jogada_computador=='Tesoura' and jogador=='Papel':
+    print(f'Referente a jogada do jogador {jogador} e a jogada do computador {jogada_computador} o COMPUTADOR VENCEU! ')
+print('Obrigado por jogar nosso game!')
 
 
 '''

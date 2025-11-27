55507fa9e1298ba09c4ef56f65bab99007c3b5d2
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-26 21:49:25 -0300

    Calculando retas para formarem um triângulo

--- Desafio09.py
+++ Desafio09.py
@@ -1,19 +1,18 @@
 from time import sleep
-
-r1= float(input('Digite aqui a medida da primeira reta:'))
-r2= float(input('Digite aqui a medida da segunda reta:'))
-r3= float(input('Digite aqui a medida da terceira reta:'))
-print('Calculando medidas...')
+reta_1= float(input('Digite aqui o comprimento da reta:'))
+reta_2= float(input('Digite aqui o comprimento da reta:'))
+reta_3= float(input('Digite aqui o comprimento da reta:'))
+print('Calculando as retas...')
 sleep(3)
-if r1+r2>r3 and r2+r3>r1 and r1+r3>r2:
-    print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo!')
-    if r1==r2==r3:
-        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triâgulo do tipo EQUILÁTERO!')
-    elif r1!=r2!=r3!=r1:
-        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo do tipo ESCALENO!')
+if reta_1+reta_2>reta_3 and reta_2+reta_3>reta_1 and reta_1+reta_3>reta_2:
+    print(f'Referente as retas {reta_1}, {reta_2} e {reta_3}, as mesmas formam um triângulo')
+    if reta_1==reta_2==reta_3:
+        print(f'Referente as retas {reta_1}, {reta_2} e {reta_3} as mesmas formam um triângulo EQUILÁTERO!')
+    elif reta_1!=reta_2!=reta_3!=reta_1:
+        print(f'Referente as reta {reta_1}, {reta_2} e {reta_3} as mesmas formam um triângulo ESCALENO!')
     else:
-        print(f'Referente as medidas informadas, as retas {r1}, {r2} e {r3} formam um triângulo ISÓSCELES!')
-print('Fim do código')
+        print(f'Referente as retas {reta_1}, {reta_2} e {reta_3} as mesmas formam um triângulo ISÓSCELES!')
+print('Fim do programa')
 
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:

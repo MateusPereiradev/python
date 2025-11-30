83275db62d4f474c8c73805eeeb6ee14a31ce531
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-30 20:29:36 -0300

    Média do aluno

--- Desafio05.py
+++ Desafio05.py
@@ -2,6 +2,8 @@ from time import sleep
 nota_1= float(input('Digite aqui o valor da primeira nota:'))
 nota_2= float(input('Digite aqui o valor da segunda nota:'))
 media= (nota_1+nota_2)/2
+print('Calculando média...')
+sleep(3)
 if media < 5.0:
     print(f'Referente a sua média de {media}, você foi \033[31mREPROVADO!')
 elif 5.0<= media < 6.9:
@@ -9,6 +11,8 @@ elif 5.0<= media < 6.9:
 elif media >=7.0:
     print(f'Referente a sua média de {media}, você está \033[32mAPROVADO!')
 print('Fim do programa de média de notas do aluno!')
+
+
 '''
 Crie um programa que leia duas notas e calcule sua média, mostrando uma mensagem no final, de acordo com a média
 atigida: 

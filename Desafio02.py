8b5a59e4eeab03e8f414199596a3bdc0dcd474d8
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-30 18:50:25 -0300

    Base de conversões  bin, oct e hex

--- Desafio02.py
+++ Desafio02.py
@@ -1,16 +1,16 @@
 num= int(input('Digite aqui um número inteiro qualquer:'))
-print('''Escolha a base de conversão:
-      [1] para binário
-      [2] para octal
-      [3] para hexadecimal''')
-escolha_usuario= int(input('Escolha aqui a sua opção de conversão:'))
-if escolha_usuario==1:
-    print(f'Referente ao número {num}, sua conversão para binário é {bin(num)[2:]}')
-elif escolha_usuario==2:
-    print(f'Referente ao número {num}, sua conversão para octal é {oct(num)[2:]}')
-elif escolha_usuario==3:
-    print(f'Referente ao número {num}, sua conversão para hexadecimal é {hex(num)[2:]}')
-print('Obrigado por usar nosso conversor!')
+print('''Escolha sua base de conversão:
+      [1] Binário
+      [2] Octal
+      [3] Hexadecimal''')
+escolha_usuário= int(input('Digite aqui sua escolha para conversão:'))
+if escolha_usuário==1:
+    print(f'Referente ao número {num}, a base convertida para binário é {bin(num)[2:]}')
+elif escolha_usuário==2:
+    print(f'Referente ao número {num}, a base convertida para octal é {oct(num)[2:]}')
+elif escolha_usuário==3:
+    print(f'Referente ao número {num}, sua base convertida para hexadecimal é {hex(num)[2:]}')
+print('Obrigado por usar nosso programa de base de conversões!')
 
 
 '''

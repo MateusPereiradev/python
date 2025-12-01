11659a7bff77a8f745c2ada2b8315648b9fb3b64
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-11-30 21:05:28 -0300

    Jogo de Jokenpô

--- Desafio10.py
+++ Desafio10.py
@@ -1,20 +1,25 @@
 from random import choice
 from time import sleep
-print('''Escolha sua jogada:
+print('''Escolha aqui sua jogada:
       Pedra
       Papel
       Tesoura''')
-jogador= str(input('Digite aqui a sua jogada Pedra, Papel ou Tesoura:')).strip().capitalize()
+escolha_jogador= str(input('Digite aqui sua escolha Pedra, Papel ou Tesoura:')).strip().capitalize()
 computador= ['Pedra', 'Papel', 'Tesoura']
-jogada_computador= choice(computador)
-if jogador==jogada_computador:
-    print(f'Referente a jogada do jogador {jogador} e a do computador {jogada_computador} houve um EMPATE!')
-if jogador=='Pedra' and jogada_computador=='Tesoura' or jogador=='Papel' and jogada_computador=='Pedra' or jogador=='Tesoura' and jogada_computador=='Papel':
-    print(f'Referente a jogada do jogador {jogador} e a jogada do computador {jogada_computador} o JOGADOR VENCEU!')
-if jogada_computador=='Pedra' and jogador=='Tesoura' or jogada_computador=='Papel' and jogador=='Pedra' or jogada_computador=='Tesoura' and jogador=='Papel':
-    print(f'Referente a jogada do jogador {jogador} e a jogada do computador {jogada_computador} o COMPUTADOR VENCEU! ')
-print('Obrigado por jogar nosso game!')
-
+escolha_computador= choice(computador)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!')
+sleep(1)
+if escolha_jogador==escolha_computador:
+    print(f'Referente a jogada do jogador de {escolha_jogador} e a do computador de {escolha_computador} houve um \033[32mEMPATE!')
+elif escolha_jogador== 'Pedra' and escolha_computador== 'Tesoura' or escolha_jogador== 'Papel' and escolha_computador== 'Pedra' or escolha_jogador== 'Tesoura' and escolha_computador== 'Papel':
+    print(f'Referente a escolha do jogador de {escolha_jogador} e a escolha do computador de {escolha_computador} o \033[32mJOGADOR VENCEU!')
+elif escolha_computador== 'Pedra' and escolha_jogador== 'Tesoura' or escolha_computador== 'Papel' and escolha_jogador=='Pedra' or escolha_computador== 'Tesoura' and escolha_jogador=='Papel':
+    print(f'Referente a escolha do jogador de {escolha_jogador} e a escola do computador de {escolha_computador} o \033[32mCOMPUTADOR VENCEU!')   
+print('Obrigado por jogador nosso jogo de Jokenpô')
 
 '''
 Crie um programa que faça o computador jogar jokenpô com você.

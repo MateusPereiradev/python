a3009d0257cb6bb904b9df39294387546f0bebfb
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-01 21:02:44 -0300

    Categoria de natação

--- Desafio06.py
+++ Desafio06.py
@@ -2,20 +2,19 @@ from time import sleep
 from datetime import date
 ano_nascimento= int(input('Digite aqui o ano de seu nascimento:'))
 idade= date.today().year-ano_nascimento
+print('Calculando categoria...')
+sleep(3)
 if idade <=9:
-    print(f'Referente a sua idade de {idade} anos, sua categoria é \033[31mMIRIN!')
-elif idade <= 14:
-    print(f'Referente a sua idade de {idade} anos, sua categoria é \033[32mINFANTIL!')
-elif idade <=19:
-    print(f'Referente a sua idade de {idade} anos, sua categoria é \033[33mJÚNIOR!')
+    print(f'Sua idade é {idade} anos, você está na categoria \033[32mMIRIN!')
+elif idade <=14:
+    print(f'Sua idade é {idade} anos, você está na categoria \033[32mINFANTIL! ')
+elif idade <= 19:
+    print(f'Sua idade é {idade} anos, você está na categoria \033[32mJÚNIOR!')
 elif idade <=20:
-    print(f'Referente a sua idade de {idade} anos, sua categoria é \033[34mSÊNIOR!')
-elif idade > 20:
-    print(f'Referente a sua idade de {idade} anos, sua categoria é \033[35mMASTER!')
-    
+    print(f'Sua idade é {idade} anos, você está na categoria \033[32mSÊNIOR!')
+elif idade >20:
+    print(f'Sua idade é {idade} anos, você está na categoria \033[32mMASTER!')
 print('Fim do programa de categoria de natação!')
-
-
 '''
 A confederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua
 categoria, de acordo com a idade:

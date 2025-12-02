516e9c5968e4be5be3897f8a166af376623b4ae1
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-02 20:00:30 -0300

    Média de nota

--- Desafio05.py
+++ Desafio05.py
@@ -1,17 +1,14 @@
 from time import sleep
-nota_1= float(input('Digite aqui o valor de sua primeira nota:'))
-nota_2= float(input('Digite aqui o valor de sua segunda nota:'))
+nota_1= float(input('Digite aqui sua primeira nota:'))
+nota_2= float(input('Digite aqui sua segunda nota:'))
 média= (nota_1+nota_2)/2
-print('Calculando seu resultado...')
-sleep(3)
-if média < 5.0:
-    print(f'Referente sua média de {média}, você foi \033[31mREPROVADO!')
+if média <5.0:
+    print(f'Referente sua média de {média} você foi \033[31mREPROVADO!')
 elif 5.0<= média <6.9:
-    print(f'Referente sua média de {média}, você está de \033[34mRECUPERAÇÃO!')
-elif média >= 7.0:
-    print(f'Referente sua média de {média}, você foi \033[32mAPROVADO!')
+    print(f'Referente sua média de {média} você está de \033[34mRECUPERAÇÃO!')
+elif média >=7.0:
+    print(f'Referente sua média de {média} você está \033[32mAPROVADO!')
 print('Fim do programa de média de nota!')
-
 
 '''
 Crie um programa que leia duas notas e calcule sua média, mostrando uma mensagem no final, de acordo com a média

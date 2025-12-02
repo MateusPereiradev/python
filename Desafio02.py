5033a352a85c3d90e3937821b5884ad8f212ebb6
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-02 19:46:49 -0300

    Conversão de números para binário, octal e hexadecimal

--- Desafio02.py
+++ Desafio02.py
@@ -1,19 +1,18 @@
 num= int(input('Digite aqui um número inteiro:'))
-print('''Escolha aqui sua base de conversão:
+print('''Escolha a base de conversão:
       [1] Binário
       [2] Octal
       [3] Hexadecimal''')
 escolha_usuário= int(input('Informe aqui sua escolha:'))
 if escolha_usuário==1:
-    print(f'Referente ao número {num} o mesmo convertido para binário é {bin(num)[2:]}')
+    print(f'Referente ao número {num}, o mesmo convertido para binário é {bin(num)[2:]}')
 elif escolha_usuário==2:
-    print(f'Referente ao número {num} o mesmo convertido para octal é {oct(num)[2:]}')
+    print(f'Referente ao número {num}, o mesmo convertido para octal é {oct(num)[2:]}')
 elif escolha_usuário==3:
-    print(f'Referente ao número {num} o mesmo convertido para hexadecimal é {hex(num)[2:]}')
+    print(f'Referente ao número {num}, o mesmo convertido para hexadecimal é {hex(num)[2:]}')
 else:
     print('Escolha inválida!')
 print('Fim do programa de conversão de números!')
-
 
 
 '''

f1902aca9bea9423d0020b4f397b131712909cb3
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-02 20:24:02 -0300

    Métodos de pagamentos

--- Desafio08.py
+++ Desafio08.py
@@ -1,24 +1,24 @@
-valor_produto= float(input('Digite aqui o valor do produto:'))
-print('''Escolha aqui a forma de pagamento:
-      [1] à vista dinheiro/cheque com 10% de desconto
-      [2] à vista no cartão com 5% de desconto
-      [3] em até 2x no cartão preço normal
-      [4] em 3x ou mais no cartão 20% de juros''')
-escolha_comprador= int(input('Escolha aqui sua forma de pagamento de 1 a 4:'))
+valor_produto= float(input('Digite aqui o valor do produto que deseja comprar:'))
+print('''Métodos de pagamento:
+      [1] À vista dinheiro/cheque com 10% de desconto
+      [2] À vista no cartão com 5% de desconto
+      [3] Em até 2x no cartão preço normal do produto
+      [4] 3x ou mais no cartão de crédito 20% de juros''')
+escolha_comprador= int(input('Digite aqui o método de pagamento que deseja:'))
 if escolha_comprador==1:
     desconto= valor_produto-valor_produto*0.10
-    print(f'Referente ao valor do produto de R${valor_produto:.2f} com o desconto de 10% ficará \033[32mR${desconto:.2f}!')
+    print(f'Referente ao valor do produto R${valor_produto:.2f} o mesmo com 10% de desconto será R${desconto:.2f}!')
 elif escolha_comprador==2:
     desconto= valor_produto-valor_produto*0.05
-    print(f'Referente ao valor do produto de R${valor_produto:.2f} com o desconto de 5% ficará \033[32mR${desconto:.2f}!')
+    print(f'Referente ao valor do produto R${valor_produto:.2f} o mesmo com 5% de desconto será R${desconto:.2f}!')
 elif escolha_comprador==3:
-    print(f'O valor do produto será o mesmo em 2x no cartão de crédito!')
+    print(f'O valor do produto será R${valor_produto:.2f}!')
 elif escolha_comprador==4:
-    juros=valor_produto+valor_produto*0.20
-    print(f'Referente ao valor do produto R${valor_produto:.2f} o mesmo com pagamento a prazo será \033[32mR${juros:.2f}!')
-    
+    juros= valor_produto+valor_produto*0.20
+    print(f'Referente ao valor do produto de R${valor_produto:.2f} o mesmo em 3x ou mais no cartão será R${juros:.2f}!')
+else:
+    print('Escolha inválida!')
 print('Obrigado por comprar em nossas lojas!')
-
 
 '''
 Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:

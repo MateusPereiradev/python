8c02abb505033d5241854aea2060bb7fefe78dc6
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-02 19:51:04 -0300

    Comparando dois números inteiros

--- Desafio03.py
+++ Desafio03.py
@@ -1,12 +1,14 @@
-num_1= int(input('Digite aqui o primeiro número:'))
-num_2= int(input('Digite aqui o segundo número:'))
-if num_1>num_2:
-    print(f'O número {num_1} é maior que o número {num_2}!')
-elif num_2>num_1:
-    print(f'O número {num_2} é maior que o número {num_1}!')
-else:
-    print(f'O número {num_1} é igual ao número {num_2}!')
+num_1= int(input('Digite aqui o primeiro número inteiro:'))
+num_2= int(input('Digite aqui o segundo número inteiro:'))
+if num_1 > num_2:
+    print(f'O primeiro número é {num_1} e o segundo número é {num_2} sendo assim o primeiro número é maior!')
+elif num_2 > num_1:
+    print(f'O primeiro número é {num_1} e o segundo número é {num_2} sendo assim o segundo número é maior!')
+elif num_1==num_2:
+    print('Os números são iguais!')
 print('Fim do programa de comparação de dois números inteiros!')
+
+
 '''
 Escreva um programa que leia dois números inteiros e compare-os mostrando na tela uma mensagem:
 O primeiro valor é maior

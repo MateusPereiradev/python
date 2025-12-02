3d2854c1636a7c209e5a1ee4f45eae6c4b818aef
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-02 20:30:58 -0300

    Triângulo, equilátero, isósceles e Escaleno

--- Desafio09.py
+++ Desafio09.py
@@ -1,14 +1,15 @@
 reta_1= float(input('Digite aqui o comprimento da primeira reta:'))
-reta_2= float(input('Digite aqui o comprimento da segunda reta:'))
-reta_3= float(input('Digite aqui o comprimento da terceira reta:'))
-if reta_1+reta_2>reta_3 and reta_2+reta_3>reta_1 and reta_3+reta_1>reta_2:
-    print(f'Referente a retas {reta_1}, {reta_2} e {reta_3} as mesmas formam um triângulo!')
+reta_2= float(input('Digite aqui o comprimeiro da segunda reta:'))
+reta_3= float(input('Digite aqui o comprimeiro da terceira reta:'))
+if reta_1+reta_2>reta_3 and reta_2+reta_3>reta_1 and reta_1+reta_3>reta_1:
+    print('As retas formam um triângulo!')
     if reta_1==reta_2==reta_3:
-        print(f'Referente as retas informadas as mesmas formam um triângulo EQUILÁTERO!')
+        print('Referente as retas informadas, as mesmas formam um triângulo Equilátero!')
     elif reta_1!=reta_2!=reta_3!=reta_1:
-        print(f'Referente as retas informadas as mesmas formam um triângulo ESCALENO!')
+        print('Referente as reta informadas, as mesmas formam um triângulo Escaleno!')
     else:
-        print('Referente as retas informadas as mesmas formam um triângulo ISÓSCELES!')
+        print('Referente as reta informadas, as mesmas formam um triângulo Isósceles!')
+print('Fim do programa de triângulos!')
 
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:

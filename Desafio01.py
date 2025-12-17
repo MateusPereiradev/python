2a3b0e45fe6afd802e7a09324569c038a9a273c9
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-17 20:34:05 -0300

    empréstimo casa

--- Desafio01.py
+++ Desafio01.py
@@ -1,14 +1,12 @@
-valor_casa= float(input('Digite aqui o valor da casa que deseja comprar:R$'))
-salário_comprador= float(input('Digite aqui o valor do seu salário:R$'))
-anos_pagamento= int(input('Informe aqui em quantos anos deseja pagar:'))
-prestação= valor_casa/(anos_pagamento*12)
-if prestação <= salário_comprador*0.30:
-    print(f'Referente ao valor da casa de R${valor_casa:.2f}, as prestações em {anos_pagamento} anos serão de R${prestação:.2f}, seu empréstimo foi \033[32mAPROVADO!')
-elif prestação > salário_comprador*0.30:
-    print(f'Referente ao valor da casa de R${valor_casa:.2f}, as prestações em {anos_pagamento} anos serão de R${prestação:.2f}, seu empréstimo foi \033[31mNEGADO!')
-print('Fim do programa de empréstimo!')
-
-
+valor_casa= float(input('Digite o valor da casa:R$'))
+salário_comprador= float(input('Digite o salário do comprador:R$'))
+anos_pagamento= int(input('Em quantos anos será pago?'))
+prestação_mensal= valor_casa/(anos_pagamento*12)
+if prestação_mensal <= salário_comprador*0.30:
+    print(f'O empréstimo foi \033[32mAPROVADO\033[m, o valor da prestação mensal será R${prestação_mensal:.2f}!')
+else:
+    print(f'O empréstimo foi \033[31mNEGADO\033[m, o valor da prestação calculada foi R${prestação_mensal:.2f}!')
+print('Fim do programa de empréstimo bancário!')
 '''
 Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
 O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.

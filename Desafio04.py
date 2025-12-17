aef412033389e8f3ac57fceb2c6816e5cc0fdc95
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-17 20:46:37 -0300

    Alistamento militar

--- Desafio04.py
+++ Desafio04.py
@@ -1,15 +1,15 @@
 from datetime import date
-ano_nascimento= int(input('Digite aqui seu ano de nascimento:'))
-idade= date.today().year-ano_nascimento
-falta= 18-idade
-passou=idade-18
+ano_nascimento= int(input('Digite o ano de nascimento do jovem:'))
+idade= date.today().year - ano_nascimento
+falta= 18 - idade
+passou= idade - 18
 if idade < 18:
-    print(f'Referente sua idade de {idade} anos, ainda faltam {falta} anos para se alistar!')
-elif idade >18:
-    print(f'Referente sua idade de {idade} anos, já passou {passou} anos para se alistar!')
-elif idade==18:
-    print(f'Referente sua idade de {idade} anos, está na hora de se alistar!')
-print('Fim do programa de alistamamento!')
+    print(f'Você tem {idade} anos, ainda faltam {falta} anos para o alistamento militar!')
+elif idade == 18:
+    print(f'Você tem {idade} anos, está na hora de se alistar ao serviço militar!')
+elif idade > 18:
+    print(f'Você tem {idade} anos, já passou {passou} anos do prazo de alistamento militar!')
+print('Fim do programa de alistamento militar!')
 
 '''
 Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a idade:

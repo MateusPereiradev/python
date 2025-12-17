630ba68f62c7b5c60b4f161256d5a778dcb519a8
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-17 20:55:08 -0300

    Categorização de atletas

--- Desafio06.py
+++ Desafio06.py
@@ -1,19 +1,17 @@
 from datetime import date
 ano_nascimento= int(input('Digite aqui seu ano de nascimento:'))
-idade= date.today().year-ano_nascimento
-if idade<=9:
-    print(f'Referente sua idade de {idade} anos, você está na categoria \033[32mMIRIN!')
-elif idade <=14:
-    print(f'Referente sua idade de {idade} anos, você está na categoria \033[32mINFANTIL!')
-elif idade <=19:
-    print(f'Referente sua idade de {idade} anos, você está na categoria \033[32mJÚNIOR!')
-elif idade<=20:
-    print(f'Referente sua idade de {idade} anos, você está na categoria \033[32mSÊNIOR!')
-elif idade > 20:
-    print(f'Referente sua idade de {idade} anos, você está na categoria \033[32mMASTER!')
-
-print('Fim do programa de categoria de natação!')
-
+idade= date.today().year - ano_nascimento
+if idade <= 9:
+    print(f'Com {idade} anos você está na categoria \033[34mMIRIM!')
+elif idade <= 14:
+    print(f'Com {idade} anos você está na categoria \033[34mINFANTIL!')
+elif idade <= 19:
+    print(f'Com {idade} anos você está na categoria \033[34mJÚNIOR!')
+elif idade ==20:
+    print(f'Com {idade} anos você está na categoria \033[34mSÊNIOR!')
+else:
+    print(f'Com {idade} anos você está na categoria \033[34mMASTER!')
+print('Fim do programa de categorização de atletas!')
 
 '''
 A confederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua

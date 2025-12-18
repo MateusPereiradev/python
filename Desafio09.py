94ee2e0f14a4a7a2cd83d601a27192961e444e27
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-17 21:17:15 -0300

    Verificação de triângulos

--- Desafio09.py
+++ Desafio09.py
@@ -1,15 +1,16 @@
-reta_1= float(input('Digite aqui o comprimento da primeira reta:'))
-reta_2= float(input('Digite aqui o comprimeiro da segunda reta:'))
-reta_3= float(input('Digite aqui o comprimeiro da terceira reta:'))
-if reta_1+reta_2>reta_3 and reta_2+reta_3>reta_1 and reta_1+reta_3>reta_1:
+reta_1= float(input('Digite o comprimento da primeira reta:'))
+reta_2= float(input('Digite o comprimento da segunda reta:'))
+reta_3= float(input('Digite o comprimento da terceira reta:'))
+if reta_1+reta_2>reta_3 and reta_1+reta_3>reta_2 and reta_2+reta_3>reta_1:
     print('As retas formam um triângulo!')
     if reta_1==reta_2==reta_3:
-        print('Referente as retas informadas, as mesmas formam um triângulo Equilátero!')
+        print('O triângulo formado é do tipo EQUILÁTERO!')
     elif reta_1!=reta_2!=reta_3!=reta_1:
-        print('Referente as reta informadas, as mesmas formam um triângulo Escaleno!')
+        print('O triângulo formado é do tipo ESCALENO!')
     else:
-        print('Referente as reta informadas, as mesmas formam um triângulo Isósceles!')
-print('Fim do programa de triângulos!')
+        print('O triângulo formado é do tipo ISÓSCELES!')
+print('Fim do programa de verificação de triângulos!')
+
 
 '''
 Refaça o Desafio 035 dos triângulos , acrescentando o recurso de mostrar que tipo de triângulo será formado:

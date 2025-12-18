d501234d19d150a8bf191a7d5d9bba3da4ed787d
MateusPereiradev <MateusPereiradev@users.noreply.github.com>
2025-12-17 21:11:26 -0300

    Valor do produto a vista, parcelado e a prazo

--- Desafio08.py
+++ Desafio08.py
@@ -1,24 +1,24 @@
-valor_produto= float(input('Digite aqui o valor do produto que deseja comprar:'))
-print('''Métodos de pagamento:
-      [1] À vista dinheiro/cheque com 10% de desconto
-      [2] À vista no cartão com 5% de desconto
-      [3] Em até 2x no cartão preço normal do produto
-      [4] 3x ou mais no cartão de crédito 20% de juros''')
-escolha_comprador= int(input('Digite aqui o método de pagamento que deseja:'))
+produto= float(input('Digite aqui o valor do produto que deseja comprar:R$'))
+print('''Escolha o método de pagamento:
+      [1] À vista dinheiro/cheque
+      [2] À vista no cartão
+      [3] Em até 2x no cartão
+      [4] 3x ou mais no cartão ''')
+escolha_comprador= int(input('Escolha a opção de pagamento de 1 a 4:'))
 if escolha_comprador==1:
-    desconto= valor_produto-valor_produto*0.10
-    print(f'Referente ao valor do produto R${valor_produto:.2f} o mesmo com 10% de desconto será R${desconto:.2f}!')
+    desconto= produto- produto*0.10
+    print(f'O valor do produto com 10% de desconto será R${desconto:.2f}!')
 elif escolha_comprador==2:
-    desconto= valor_produto-valor_produto*0.05
-    print(f'Referente ao valor do produto R${valor_produto:.2f} o mesmo com 5% de desconto será R${desconto:.2f}!')
+    desconto= produto-produto*0.05
+    print(f'O valor do produto com 5% de desconto será R${desconto:.2f}!')
 elif escolha_comprador==3:
-    print(f'O valor do produto será R${valor_produto:.2f}!')
+    print(f'O valor do produto será parcelado em até 2x assim o seu preço será R${produto:.2f}!')
 elif escolha_comprador==4:
-    juros= valor_produto+valor_produto*0.20
-    print(f'Referente ao valor do produto de R${valor_produto:.2f} o mesmo em 3x ou mais no cartão será R${juros:.2f}!')
+    juros= produto+produto*0.20
+    print(f'O valor do produto com 20% de juros será R${juros:.2f} dividido de 3x ou mais!')
 else:
-    print('Escolha inválida!')
-print('Obrigado por comprar em nossas lojas!')
+    print('Opção inválida de pagamento, tente novamente!')
+print('Fim do programa de cálculo de preço final do produto!')
 
 '''
 Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
